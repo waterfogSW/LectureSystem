438e9cf55c997161335afae9480192ca5ff6edaa
San Kim <waterfog9580@gmail.com>
2023-10-24 20:32:39 +0900

    ✨ Add functionality to read environment variables from .env file
    A utility function 'loadEnvVars()' has been added to read environment variables from a '.env' file. to populate the process.env object with values from the '.env' file. These changes allow for easier configuration of environment-specific variables, improving project flexibility and environment setup.

--- src/main/index.ts
+++ src/main/index.ts
@@ -6,7 +6,9 @@ import * as bodyParser from 'body-parser';
 import container from './common/config/ioc.config';
 import { routerConfig } from './common/config/router.config';
 import { errorHandler } from './common/middleware/error-handler.middleware';
+import { loadEnvVars } from './common/util/env.util';
 
+loadEnvVars();
 const port: string = process.env.PORT || String(3000);
 const server: Server = new Server(container);
 
