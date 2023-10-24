7efc3fdd02ef4e46e393e086d7ac265f343e1b95
San Kim <waterfog9580@gmail.com>
2023-10-24 18:19:16 +0900

    ♻️ Refactor routing setup to use separate config
    Refactored our codebase so that our routing setup uses a separate configuration file, router.config.ts. This makes routing setup more organized, maintainable, and scalable. A doAsync function was added to handle async errors in the router config, improving error handling.

--- src/main/index.ts
+++ src/main/index.ts
@@ -4,8 +4,7 @@ import { Application } from 'express';
 import { Server } from './server';
 import * as bodyParser from 'body-parser';
 import container from './common/config/ioc.config';
-import { StudentController } from './controller/student.controller';
-import TYPES from './common/type/types';
+import { routerConfig } from './common/config/router.config';
 
 const port: string = process.env.PORT || String(3000);
 const server: Server = new Server(container);
@@ -13,9 +12,7 @@ const server: Server = new Server(container);
 server.setConfig((app: Application): void => {
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
-
-  const studentController: StudentController = container.get<StudentController>(TYPES.StudentController);
-  app.post('/api/students', studentController.createStudent.bind(studentController));
+  routerConfig(app);
 });
 const app: Application = server.build();
 
