26e973cb8408a1b7753f58111115f148c0228958
San Kim <waterfog9580@gmail.com>
2023-10-26 21:23:18 +0900

    ✨ Add Lecture-related binding types
    Expanded the list of BindingTypes in the application's codebase to include Lecture related classes (like LectureController, LectureService, etc). This was done to ensure inversion of control and dependency injection for the lecture related components of the system.

--- src/main/common/constant/BindingTypes.ts
+++ src/main/common/constant/BindingTypes.ts
@@ -1,7 +1,15 @@
 export const BindingTypes = {
   ConnectionPool: Symbol('ConnectionPool'),
+
+  // student
   StudentController: Symbol('StudentController'),
   StudentService: Symbol('StudentService'),
   StudentRepository: Symbol('StudentRepository'),
   StudentDTOMapper: Symbol('StudentDTOMapper'),
+
+  // lecture
+  LectureController: Symbol('LectureController'),
+  LectureService: Symbol('LectureService'),
+  LectureRepository: Symbol('LectureRepository'),
+  LectureDTOMapper: Symbol('LectureDTOMapper'),
 };
