04b5ba92c952d6465b4f831f537830cbe0ff7680
San Kim <waterfog9580@gmail.com>
2023-10-28 00:18:28 +0900

    ✅ Add tests for InstructorRepository
    Added unit tests for the InstructorRepository class to check the functionality of the 'findById' method. These tests check whether the method can correctly return null when no instructor exists with the provided ID, and whether it can return the correct instructor when there is a match for the provided ID.

---
+++ src/test/repository/InstructorRepository.test.ts
@@ -0,0 +1,44 @@
+import 'reflect-metadata';
+import { beforeEach, describe, expect, it, jest } from '@jest/globals';
+import { InstructorRepository } from '../../main/repository/InstructorRepository';
+import { Instructor } from '../../main/domain/Instructor';
+
+
+describe('InstructorRepository', () => {
+  let instructorRepository: InstructorRepository;
+  let mockConnection: any;
+
+  beforeEach(() => {
+    instructorRepository = new InstructorRepository();
+    mockConnection = { execute: jest.fn() };
+  });
+
+  describe('findById', () => {
+
+    it('ID값에 해당하는 강사가 없다면 null을 반환한다.', async () => {
+      // given
+      mockConnection.execute.mockResolvedValue([[], []]);
+
+      // when
+      const instructor: Instructor | null = await instructorRepository.findById(1, mockConnection);
+
+      // then
+      expect(instructor).toBeNull();
+    });
+
+    it('ID값에 해당하는 강사를 반환한다', async () => {
+      // given
+      const findId = 1;
+      const mockInstructorData = [
+        { id: findId, name: 'Test Instructor', created_at: '2021-08-01 00:00:00', updated_at: '2021-08-01 00:00:00' },
+      ];
+      mockConnection.execute.mockResolvedValue([mockInstructorData, []]); // 모의 execute 함수가 결과를 반환
+
+      // when
+      const instructor: Instructor | null = await instructorRepository.findById(findId, mockConnection);
+
+      // Assert
+      expect(instructor?.id!!).toBe(findId);
+    });
+  });
+});
