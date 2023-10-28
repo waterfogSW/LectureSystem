d529d3253ff3230ae97ec76d20fbbcfe605a358c
San Kim <waterfog9580@gmail.com>
2023-10-28 14:44:24 +0900

    ✨ Add LectureType with LectureCategory, LectureSearchType, LectureOrderType
    New file LectureType.ts is introduced which adds three distinct lecture types - LectureCategory, LectureSearchType, and LectureOrderType. They are created to cater to the versatile requirements of the application such as search functionality and categorization. This will make the application more robust and scalable.

---
+++ src/main/domain/LectureType.ts
@@ -0,0 +1,23 @@
+type ReadonlyRecord<K extends string, V> = Readonly<Record<K, V>>;
+export type LectureCategoryNames = 'WEB' | 'APP' | 'GAME' | 'ALGORITHM' | 'INFRA' | 'DATABASE'
+export const LectureCategory: ReadonlyRecord<LectureCategoryNames, LectureCategoryNames> = {
+  WEB: 'WEB',
+  APP: 'APP',
+  GAME: 'GAME',
+  ALGORITHM: 'ALGORITHM',
+  INFRA: 'INFRA',
+  DATABASE: 'DATABASE',
+};
+
+export type LectureSearchTypeNames = 'TITLE' | 'INSTRUCTOR' | 'STUDENT_ID'
+export const LectureSearchType: ReadonlyRecord<LectureSearchTypeNames, LectureSearchTypeNames> = {
+  TITLE: 'TITLE',
+  INSTRUCTOR: 'INSTRUCTOR',
+  STUDENT_ID: 'STUDENT_ID',
+};
+
+export type LectureOrderTypeNames = 'RECENT' | 'ENROLLMENTS'
+export const LectureOrderType: ReadonlyRecord<LectureOrderTypeNames, LectureOrderTypeNames> = {
+  RECENT: 'RECENT',
+  ENROLLMENTS: 'ENROLLMENTS',
+};
