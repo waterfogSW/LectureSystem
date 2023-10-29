72a0b7be55d2ff942d3c4bf165470064ee71d625
San Kim <waterfog9580@gmail.com>
2023-10-29 19:39:03 +0900

    🔧 Add connection pool settings to DatabaseConfig
    This commit enhances database connection management by adding connection pool settings in the DatabaseConfig.ts file. These new settings include options for connections limit, max idle connections, idle timeout, queue limit, as well as enabling keep-alive feature. This improvement aims to optimize resource usage and improve overall performance of database interactions.

--- src/main/common/config/DatabaseConfig.ts
+++ src/main/common/config/DatabaseConfig.ts
@@ -8,6 +8,13 @@ const connectionPoolConfig = {
   user: 'root',
   password: 'root',
   timezone: '+09:00', // 한국 시간
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10,
+  idleTimeout: 60000,
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
 };
 
 @injectable()
