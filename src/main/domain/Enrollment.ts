21467bfaed4106353fc8b8ca3da8a84291287aae
San Kim <waterfog9580@gmail.com>
2023-10-30 12:52:42 +0900

    ✨ Add Enrollment entity for handling student enrollments
    This commit introduces a new class, Enrollment, in the 'domain' directory. It's an entity representing the enrollment of a student for lecture. This class includes lecture and student IDs both having validation rules ensuring they are positive. This will facilitate the handling and validation of enrollments in the backend logic.

---
+++ src/main/domain/Enrollment.ts
@@ -0,0 +1,33 @@
+import { BaseEntity, Id } from '../common/entity/BaseEntity';
+import { IsPositive } from 'class-validator';
+import { validateClass } from '../common/util/ClassValidateUtil';
+
+export class Enrollment extends BaseEntity {
+
+  @IsPositive({ message: '강의 아이디는 0보다 커야 합니다.' })
+  private readonly _lectureId: number;
+
+  @IsPositive({ message: '학생 아이디는 0보다 커야 합니다.' })
+  private readonly _studentId: number;
+
+  constructor(
+    id: Id,
+    lectureId: number,
+    studentId: number,
+    createdAt: Date,
+    updatedAt: Date,
+  ) {
+    super(id, createdAt, updatedAt);
+    this._lectureId = lectureId;
+    this._studentId = studentId;
+    validateClass(this);
+  }
+
+  public get lectureId(): number {
+    return this._lectureId;
+  }
+
+  public get studentId(): number {
+    return this._studentId;
+  }
+}
