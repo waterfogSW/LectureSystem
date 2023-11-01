f6c41b90a6955a67f50a364fee68c99a7d399828
San Kim <waterfog9580@gmail.com>
2023-11-01 23:46:43 +0900

    ♻️ Refactor Student unit tests for clarity
    The descriptions for each test in the Student unit test suite were updated for better readability and understanding. Instead of having a single 'it' block describing what the test does, each 'it' block is now enclosed within a 'describe' block categorizing it

--- src/test/model/Student.test.ts
+++ src/test/model/Student.test.ts
@@ -5,52 +5,54 @@ import { IllegalArgumentException } from '../../main/common/exception/IllegalArg
 
 describe('Student', () => {
 
-  it('새로운 수강생을 생성한다.', () => {
-    // given
-    const nickname: string = 'test';
-    const email: string = 'test@example.com';
+  describe('create', () => {
+    it('[Success] 새로운 수강생을 생성한다', () => {
+      // given
+      const nickname: string = 'test';
+      const email: string = 'test@example.com';
 
-    // when
-    const student: Student = Student.create(nickname, email);
+      // when
+      const student: Student = Student.create(nickname, email);
 
-    // then
-    expect(student.nickname).toBe(nickname);
-    expect(student.email).toBe(email);
-  });
+      // then
+      expect(student.nickname).toBe(nickname);
+      expect(student.email).toBe(email);
+    });
 
-  it.each([2, 11])('닉네임은 3자 이상 10자 이하만 가능하다. (닉네임 길이: %i)', (length: number) => {
-    // given
-    const nickname: string = 'a'.repeat(length);
-    const email: string = 'test@example.com';
+    it.each([2, 11])('[Failure] 닉네임이 3자 이상 10자 이하가 아니면 예외를 던진다 (닉네임 길이: %i)', (length: number) => {
+      // given
+      const nickname: string = 'a'.repeat(length);
+      const email: string = 'test@example.com';
 
-    // when, then
-    expect(() => Student.create(nickname, email)).toThrowError(IllegalArgumentException);
-  });
+      // when, then
+      expect(() => Student.create(nickname, email)).toThrowError(IllegalArgumentException);
+    });
 
-  it('닉네임은 공백일 수 없다.', () => {
-    // given
-    const nickname: string = '';
-    const email: string = 'test@example.com';
+    it('[Failure] 닉네임이 공백이면 예외를 던진다', () => {
+      // given
+      const nickname: string = '';
+      const email: string = 'test@example.com';
 
-    // when, then
-    expect(() => Student.create(nickname, email)).toThrowError(IllegalArgumentException);
-  });
+      // when, then
+      expect(() => Student.create(nickname, email)).toThrowError(IllegalArgumentException);
+    });
 
-  it('이메일은 공백일 수 없다.', () => {
-    // given
-    const nickname: string = 'test';
-    const email: string = '';
+    it('[Failure] 이메일이 공백이면 예외를 던진다', () => {
+      // given
+      const nickname: string = 'test';
+      const email: string = '';
 
-    // when, then
-    expect(() => Student.create(nickname, email)).toThrowError(IllegalArgumentException);
-  });
+      // when, then
+      expect(() => Student.create(nickname, email)).toThrowError(IllegalArgumentException);
+    });
 
-  it('이메일은 이메일 형식이어야 한다.', () => {
-    // given
-    const nickname: string = 'test';
-    const email: string = 'test';
+    it('[Failure] 이메일이 형식에 맞지 않으면 예외를 던진다', () => {
+      // given
+      const nickname: string = 'test';
+      const email: string = 'test';
 
-    // when, then
-    expect(() => Student.create(nickname, email)).toThrowError(IllegalArgumentException);
+      // when, then
+      expect(() => Student.create(nickname, email)).toThrowError(IllegalArgumentException);
+    });
   });
 });
