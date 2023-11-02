a0894b11b3d7e1e592147d80fa082f9af6539a74
San Kim <waterfog9580@gmail.com>
2023-11-02 11:35:10 +0900

    ✨ Add MockTransaction utility for testing
    This commit adds a utility, MockTransaction.ts, for mocking out transaction-related functionalities. It uses jest.spyOn to mock the 'get' method on the dependency container object. The change facilitates more efficient and isolated testing by allowing test cases to create a mock transaction context, avoiding the need for real database connections.

---
+++ src/test/util/MockTransaction.ts
@@ -0,0 +1,20 @@
+import { jest } from '@jest/globals';
+import { container } from '../../main/common/config/ContainerConfig';
+import { BindingTypes } from '../../main/common/constant/BindingTypes';
+
+export function initializeMockTransactionContext() {
+  jest.spyOn(container, 'get').mockImplementation((bindingType) => {
+    if (bindingType === BindingTypes.ConnectionPool) {
+      return {
+        getConnection: jest.fn().mockImplementation(() => {
+          return {
+            beginTransaction: jest.fn(),
+            commit: jest.fn(),
+            rollback: jest.fn(),
+            release: jest.fn(),
+          };
+        }),
+      };
+    }
+  });
+}
